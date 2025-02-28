9a118a1e5cc002732f90be5481e1c06e54852ce8
jacobo <jacoboburbano2004@gmail.com>
2025-02-28 12:41:00 -0500

    Commit: Database migration to render

--- restful/settings.py
+++ restful/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+import dj_database_url
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,11 +84,15 @@ WSGI_APPLICATION = 'restful.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('PGDATABASE'),
+        'USER': os.getenv('PGPASSWORD'),
+        'PASSWORD': os.getenv('PGHOST'),
+        'PORT': os.getenv('PGPORT')
     }
 }
-
+database_url = os.getenv('DATABASE_URL')
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
